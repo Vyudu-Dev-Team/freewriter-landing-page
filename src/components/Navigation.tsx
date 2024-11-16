20eab2bf9adfcfd829e8f91d5d812835f0c9e4d3
Jeremy Williams <jeremy@vyudu.com>
2024-11-16 01:34:45 -0500

    removed menu navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -16,29 +16,7 @@ const links: NavLink[] = [
 ];
 
 const Navigation: React.FC = () => {
-  return (
-    <motion.nav 
-      className="fixed top-8 right-8 z-40"
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.5 }}
-    >
-      <ul className="flex flex-col gap-4">
-        {links.map((link) => (
-          <li key={link.href}>
-            <motion.a 
-              href={link.href}
-              className="flex items-center gap-2 text-primary-lime hover:text-primary-purple transition-colors duration-300 body-text tracking-[0.2em]"
-              whileHover={{ x: -5 }}
-            >
-              <link.icon className="w-5 h-5" />
-              <span>{link.label}</span>
-            </motion.a>
-          </li>
-        ))}
-      </ul>
-    </motion.nav>
-  );
+  return null;
 };
 
 export default Navigation;
